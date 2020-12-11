9e7f5ef6c769c9ff09e0fab2841b3ac1eeee819f
Kevyn Paola <paolakevin@hitmail.com>
2020-12-11 18:09:25 -0500

    avance de las vistas

--- app/src/main/java/com/example/taller_listv/MainActivity.java
+++ app/src/main/java/com/example/taller_listv/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.taller_listv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView opciones;
+    private ListView areas;
+    private ListView volumenes;
+
+    public MainActivity(ListView opciones) {
+        this.opciones = opciones;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
